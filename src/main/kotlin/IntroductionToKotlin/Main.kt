084764df9946d2f0a4827d3d1be35918ad9b70b1
Denis Polovkov <denis.polovkoff@gmail.com>
2023-09-20 19:07:53 +0300

    Replace text message

--- src/main/kotlin/IntroductionToKotlin/Main.kt
+++ src/main/kotlin/IntroductionToKotlin/Main.kt
@@ -3,7 +3,7 @@ package IntroductionToKotlin
  * Write your first program in Kotlin
  */
 fun main() {
-    println("Happy Birthday!")
-    println("Jhansi")
-    println("You are 25!")
+    println("Happy Birthday, Rover!")
+    println("You are already 5!")
+    println("5 is the very best age to celebrate!")
 }
