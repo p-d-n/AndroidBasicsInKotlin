f80feb80963675a0632cc125734f40deffa1b60c
Denis Polovkov <denis.polovkoff@gmail.com>
2023-09-20 20:43:15 +0300

    Create a cake with the layers and candles

--- src/main/kotlin/introductionToKotlin/Main.kt
+++ src/main/kotlin/introductionToKotlin/Main.kt
@@ -3,16 +3,40 @@ package introductionToKotlin
  * Write your first program in Kotlin
  */
 fun main() {
-    val border = "`-._,-'"
-    val timesToRepeat = 4
-    printBorder(border, timesToRepeat)
-    println("  Happy Birthday, Jhansi!")
-    printBorder(border, timesToRepeat)
+    val age = 24
+    val layers = 5
+    printCakeCandles(age)
+    printCakeTop(age)
+    printCakeBottom(age, layers)
 }
 
-fun printBorder(border: String, timesToRepeat: Int) {
-    repeat(timesToRepeat) {
-        print(border)
+fun printCakeCandles(age: Int) {
+    print (" ")
+    repeat(age) {
+        print(",")
+    }
+    println() // Print an empty line
+
+    print(" ") // Print the inset of the candles on the cake
+    repeat(age) {
+        print("|")
     }
     println()
 }
+
+fun printCakeTop(age: Int) {
+    repeat(age + 2) {
+        print("=")
+    }
+    println()
+}
+
+fun printCakeBottom(age: Int, layers: Int) {
+    repeat(layers) {
+        repeat(age + 2) {
+            print("@")
+        }
+        println()
+    }
+}
+
