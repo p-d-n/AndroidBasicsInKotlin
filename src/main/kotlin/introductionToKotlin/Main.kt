b06f927aed39b39a24559d68b0ab7166a68aa567
Denis Polovkov <denis.polovkoff@gmail.com>
2023-09-20 20:17:16 +0300

    Create and use variables

--- src/main/kotlin/introductionToKotlin/Main.kt
+++ src/main/kotlin/introductionToKotlin/Main.kt
@@ -3,7 +3,10 @@ package introductionToKotlin
  * Write your first program in Kotlin
  */
 fun main() {
-    println("Happy Birthday, Rover!")
+    val age = 5 * 365
+    val name = "Rover"
+
+    println("Happy Birthday, $name!")
 
     // Let's print a cake!
     println("   ,,,,,   ")
@@ -16,6 +19,6 @@ fun main() {
     // This prints an empty line.
     println("")
 
-    println("You are already 5!")
-    println("5 is the very best age to celebrate!")
+    println("You are already $age days old, $name!")
+    println("$age days old is the very best age to celebrate!")
 }
